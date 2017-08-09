1132f392d9560d2e1baa3487726838cba2f81670
ykushev <ykushev@localhost.localdomain>
2017-08-10 03:57:33 +0700

    rest done

--- lib/marfa/helpers/http/rest.rb
+++ lib/marfa/helpers/http/rest.rb
@@ -8,16 +8,17 @@ module Marfa
       # Helpers to use Rest requests
       class Rest
 
-        # get request
+        # GET request
         # @param url [String] - url
         # @param headers [Hash] - headers hash
-        # @param json_parse [Hash] - parse json options
-        # @param block [Hash] - other options
-        def self.get (url, headers={}, &block)
+        # @example
+        #   response = Rest.get(url, headers)
+        # @return response [RestClient::Response]
+        def self.get (url, headers={})
           p "REST GET url    = #{url}" if Marfa.config.logging_level > 0
-          p "REST GET headers= #{headers}" if Marfa.config.logging_level > 1
+          p "REST GET headers= #{headers}" if Marfa.config.logging_level > 0
 
-          response = RestClient.get(url, headers, &block)
+          response = RestClient.get(url, headers)
           p "REST GET code   = #{response.code}" if Marfa.config.logging_level > 0
 
           p response.body if Marfa.config.logging_level > 1
@@ -25,27 +26,111 @@ module Marfa
           response
         end
 
-        def self.post (url, payload, headers={}, &block)
-          p "REST POS url    = #{url}" if Marfa.config.logging_level > 0
-          if Marfa.config.logging_level > 1
-            p "REST POS headers= #{headers}"
-            p 'REST POS payload= '
-            pp payload
-          end
+        # HEAD request
+        # @param url [String] - url
+        # @param headers [Hash] - headers hash
+        # @example
+        #   response = Rest.head(url)
+        # @return response [RestClient::Response]
+        def self.head (url, headers={})
+          p "REST HEA url    = #{url}" if Marfa.config.logging_level > 0
+          p "REST HEA headers= #{headers}" if Marfa.config.logging_level > 0
 
-          response = RestClient.post(url, payload, headers, &block)
-          p "REST POS code   = #{response.code}" if Marfa.config.logging_level > 0
+          response = RestClient.head(url, headers)
+          p "REST HEA code   = #{response.code}" if Marfa.config.logging_level > 0
+
           p response.body if Marfa.config.logging_level > 1
 
           response
         end
 
+        # POST request
+        # @param url [String] - url
+        # @param payload [String] - request body
+        # @param headers [Hash] - headers hash
+        # @param block [Proc] - block code to run
+        # @example
+        #   response = Rest.post(url, payload)
+        #   Rest.post(url, payload) do |response|
+        # @return response [RestClient::Response]
+        def self.post (url, payload, headers={}, &block)
+          p "REST POS url    = #{url}" if Marfa.config.logging_level > 0
+          if Marfa.config.logging_level > 0
+            p "REST POS headers= #{headers}"
+            p "REST POS payload= #{payload}"
+          end
+
+          if block.nil?
+            response = RestClient.post(url, payload, headers)
+            p "REST POS code   = #{response.code}" if Marfa.config.logging_level > 0
+
+            p response.body if Marfa.config.logging_level > 1
+
+            return response
+          else
+            RestClient::Request.execute(
+                :method => :post,
+                :url => url,
+                :payload => payload,
+                :headers => headers) do |response|
+              p "REST POS code   = #{response.code}" if Marfa.config.logging_level > 0
+              p response.body if Marfa.config.logging_level > 1
+
+              block.call(response)
+            end
+          end
+        end
+
+        # PUT request
+        # @param url [String] - url
+        # @param payload [String] - request body
+        # @param headers [Hash] - headers hash
+        # @param block [Proc] - block code to run
+        # @example
+        #   response = Rest.put(url, payload)
+        #   Rest.put(url, payload) do |response|
+        # @return response [RestClient::Response]
+        def self.put (url, payload, headers={}, &block)
+          p "REST PUT url    = #{url}" if Marfa.config.logging_level > 0
+          if Marfa.config.logging_level > 0
+            p "REST PUT headers= #{headers}"
+            p "REST PUT payload= #{payload}"
+          end
+
+          if block.nil?
+            response = RestClient.put(url, payload, headers)
+            p "REST PUT code   = #{response.code}" if Marfa.config.logging_level > 0
+
+            p response.body if Marfa.config.logging_level > 1
+
+            return response
+          else
+            RestClient::Request.execute(
+                :method => :put,
+                :url => url,
+                :payload => payload,
+                :headers => headers) do |response|
+              p "REST PUT code   = #{response.code}" if Marfa.config.logging_level > 0
+              p response.body if Marfa.config.logging_level > 1
+
+              block.call(response)
+            end
+          end
+        end
+
+        # DELETE request
+        # @param url [String] - url
+        # @param payload [String] - request body
+        # @param headers [Hash] - headers hash
+        # @param block [Proc] - block code to run
+        # @example
+        #   Rest.delete(url, payload, headers) do |response|
+        # @return response [RestClient::Response]
         def self.delete (url, payload, headers={}, &block)
           p "REST DEL url    = #{url}" if Marfa.config.logging_level > 0
-          if Marfa.config.logging_level > 1
+          if Marfa.config.logging_level > 0
             p "REST DEL headers= #{headers}"
-            p 'REST DEL payload= '
-            pp payload
+            p "REST DEL payload= #{payload}"
           end
 
           RestClient::Request.execute(
@@ -57,10 +142,9 @@ module Marfa
 
             p response.body if Marfa.config.logging_level > 1
 
-            return response
+            block.call(response)
           end
         end
-
       end
     end
   end
